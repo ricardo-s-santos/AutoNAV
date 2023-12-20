4ce65f429db5a273b845da0883d3b2c9eace6775
Ricardo Santos <ricardosantoss@live.com.pt>
2023-12-20 10:54:33 +0000

    Fix variable names in cli.py

--- autonav/cli.py
+++ autonav/cli.py
@@ -38,24 +38,24 @@ def _main():
     if algorithm == "1" or algorithm == "2":
         default = input("Do you want to use the default anchor number and positions (y/n)?\n")
         if default == "y" or default == "Y":
-            B = 200
-            N = 8
+            b = 200
+            n = 8
             a_i = array(
                 [
                     [0, 0, 0],
-                    [0, B, 0],
-                    [B / 2, 0, 0],
-                    [B / 2, B, 0],
-                    [0, 0, B / 8],
-                    [0, B, B / 8],
-                    [B / 2, 0, B / 8],
-                    [B / 2, B, B / 8],
+                    [0, b, 0],
+                    [b / 2, 0, 0],
+                    [b / 2, b, 0],
+                    [0, 0, b / 8],
+                    [0, b, b / 8],
+                    [b / 2, 0, b / 8],
+                    [b / 2, b, b / 8],
                 ]
             ).T
         elif default == "n" or default == "N":
-            N = int(input("Please insert the number of Anchors (N)\n"))
+            n = int(input("Please insert the number of Anchors (N)\n"))
             a_i = []
-            for i in range(0, N):
+            for i in range(0, n):
                 aux = input(
                     f"Please insert the positions of anchor {i + 1} in the following format: x1, y1, z1\n"
                 )
@@ -70,14 +70,14 @@ def _main():
         if algorithm == "1":
             print("Running GTRS...")
             start_time = time.time()
-            estimated_trajectory = gtrs(a_i, N, K, sigma, destinations, [10, 10, 5])
+            estimated_trajectory = gtrs(a_i, n, K, sigma, destinations, [10, 10, 5])
             exec_time = time.time() - start_time
             print(f"GTRS finished in {exec_time:0,.2f} seconds.")
             plot_trajectories(destinations, estimated_trajectory)
         elif algorithm == "2":
             print("Running WLS...")
             start_time = time.time()
-            estimated_trajectory = wls(a_i, N, K, sigma, destinations, [10, 10, 5])
+            estimated_trajectory = wls(a_i, n, K, sigma, destinations, [10, 10, 5])
             exec_time = time.time() - start_time
             print(f"WLS finished in {exec_time:0,.2f} seconds.")
             plot_trajectories(destinations, estimated_trajectory)
