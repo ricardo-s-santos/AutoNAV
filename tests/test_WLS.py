ceaa6f08c3221dfc603c72373e983ed75344d32c
Ricardo Santos <ricardosantoss@live.com.pt>
2024-01-07 16:28:18 +0000

    Add cov.yaml

--- tests/test_WLS.py
+++ tests/test_WLS.py
@@ -6,7 +6,6 @@ from autonav.WLS import wls
 from numpy.testing import assert_allclose
 
 
-
 @pytest.mark.critical()
 def test_wls_no_noise(default_values, expected_trajectories_wls):
     This test pretends to see if the algorithm is correctly implemented by setting the noise to zero.
