db44d5a6dd26500d3590f99905b6c0cab876dddb
ricardo-s-santos <56162588+ricardo-s-santos@users.noreply.github.com>
2024-01-26 12:31:59 +0000

    Deploying to gh-pages from @ ricardo-s-santos/AutoNAV@187551d7eeab24de3386e7fd816c82bb395c5c21 🚀

---
+++ generated/gallery/plot_rmse.py
@@ -0,0 +1,33 @@
+"""# Extract RMSE and ARMSE metrics
+
+"""
+
+from autonav import gtrs, wls
+from autonav.file_handlers import readpathfile
+from autonav.metrics import compute_armse
+from autonav.plots import plot_rmse
+from numpy import array
+
+b = 200
+n = 8
+a_i = array(
+    [
+    [0, 0, 0],
+    [0, b, 0],
+    [b / 2, 0, 0],
+    [b / 2, b, 0],
+    [0, 0, b / 8],
+    [0, b, b / 8],
+    [b / 2, 0, b / 8],
+    [b / 2, b, b / 8],]
+    ).T
+k = 50
+sigma = 1
+initial_uav_position = [10, 10, 5]
+destinations = readpathfile("Path.txt")
+
+[estimated_trajectory_gtrs, true_trajectory_gtrs] = gtrs(a_i, n, k, sigma, destinations, initial_uav_position)
+[estimated_trajectory_wls, true_trajectory_wls] = wls(a_i, n, k, sigma, destinations, initial_uav_position)
+print(f'GTRS ARMSE: {compute_armse(estimated_trajectory_gtrs, true_trajectory_gtrs)}')
+print(f'WLS ARMSE: {compute_armse(estimated_trajectory_wls, true_trajectory_wls)}')
+plot_rmse([estimated_trajectory_gtrs, estimated_trajectory_wls],[true_trajectory_gtrs, true_trajectory_wls])
---
+++ generated/gallery/plot_wls_1.py
@@ -0,0 +1,29 @@
+"""# WLS example in 3D
+
+"""
+
+from autonav import wls
+from autonav.file_handlers import readpathfile
+from autonav.plots import plot_trajectories
+from numpy import array
+
+b = 200
+n = 8
+a_i = array(
+    [
+    [0, 0, 0],
+    [0, b, 0],
+    [b / 2, 0, 0],
+    [b / 2, b, 0],
+    [0, 0, b / 8],
+    [0, b, b / 8],
+    [b / 2, 0, b / 8],
+    [b / 2, b, b / 8],]
+    ).T
+k = 50
+sigma = 1
+initial_uav_position = [10, 10, 5]
+destinations = readpathfile("Path.txt")
+
+[estimated_trajectory, true_trajectory] = wls(a_i, n, k, sigma, destinations, initial_uav_position)
+plot_trajectories(destinations, [estimated_trajectory], a_i, ['WLS'])
