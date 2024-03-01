a65af8b19bdf246a7ce6ac9360076f4f6949dd1c
Ricardo Santos <ricardosantoss@live.com.pt>
2024-03-01 17:45:39 +0000

    Improve WLS and plt_rmse example.

--- docs/docs/examples/plot_wls_1.py
+++ docs/docs/examples/plot_wls_1.py
@@ -2,11 +2,15 @@
 
 """
 
+#%%
+# First, the required dependencies are imported:
 from autonav import wls
 from autonav.file_handlers import readpathfile
 from autonav.plots import plot_trajectories
 from numpy import array
 
+#%%
+# The next step is to define the simulation parameters:
 b = 200
 n = 8
 a_i = array(
@@ -24,6 +28,9 @@ k = 50
 sigma = 1
 initial_uav_position = [10, 10, 5]
 destinations = readpathfile("Path.csv")
+
+#%%
+# Finally, one can invoke the [`wls`] and function and plot the estimated trajectory:
 
 [estimated_trajectory, true_trajectory] = wls(a_i, n, k, sigma, destinations, initial_uav_position)
 plot_trajectories(destinations, [estimated_trajectory], a_i, ['WLS'])
