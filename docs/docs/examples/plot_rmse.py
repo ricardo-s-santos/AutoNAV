0040d021af4a4fc41c6a013a63fed6121ad02e5c
Ricardo Santos <ricardosantoss@live.com.pt>
2024-03-03 22:07:24 +0000

    Add comments to examples

--- docs/docs/examples/plot_rmse.py
+++ docs/docs/examples/plot_rmse.py
@@ -13,11 +13,8 @@ from numpy import array
 #%%
 # The next step is to define the simulation parameters:
 
-# Area border
 b = 200
-# Number of anchors
 n = 8
-# Position of the anchors
 a_i = array(
     [
     [0, 0, 0],
@@ -29,23 +26,16 @@ a_i = array(
     [b / 2, 0, b / 8],
     [b / 2, b, b / 8],]
     ).T
-# Number of measurement samples
 k = 50
-# Noise standard deviation
 sigma = 1
-# Maximum velocity allowed to the UAV
 v_max = b / 100
-# Distance threshold
 tau = b / 50
-# Smoothing factor
-gamma = b / 10
-# Initial position of the UAV
+gamma = b / 100
 initial_uav_position = [10, 10, 5]
-# File containing the waypoints
 destinations = readpathfile("Path.csv")
 
 #%%
-# Invoke the [`gtrs`] and [`wls`] functions and afterwards the [`compute_armse`] and [`plot_rmse`] as follows to compute the ARMSE and plot the RMSE for both algorithms, respectively:
+# Invoke the [`gtrs`] and [`wls`] functions and afterwards the [`compute_armse`] and [`plot_rmse`] as follows to compute the ARMSE and plot the RMSE, respectively:
 
 [estimated_trajectory_gtrs, true_trajectory_gtrs] = gtrs(a_i, n, k, sigma, destinations, initial_uav_position, v_max, tau, gamma)
 [estimated_trajectory_wls, true_trajectory_wls] = wls(a_i, n, k, sigma, destinations, initial_uav_position, v_max, tau, gamma)
