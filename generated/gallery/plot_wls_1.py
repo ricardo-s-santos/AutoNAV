2f5af70e09f41ac1c58875fbb1018bb0826b1937
ricardo-s-santos <56162588+ricardo-s-santos@users.noreply.github.com>
2024-05-17 16:38:54 +0000

    Deploying to gh-pages from @ ricardo-s-santos/AutoNAV@bbc5e36aaa784ca74aa37cd5c3f9558f565d5403 🚀

--- generated/gallery/plot_wls_1.py
+++ generated/gallery/plot_wls_1.py
@@ -2,14 +2,14 @@
 
 """
 
-#%%
+# %%
 # First, the required dependencies are imported:
 from autonav import wls
 from autonav.file_handlers import readpathfile
 from autonav.plots import plot_trajectories
 from numpy import array
 
-#%%
+# %%
 # The next step is to define the simulation parameters:
 
 # Area border
@@ -19,19 +19,18 @@ n = 8
 # Position of the anchors
 a_i = array(
     [
-    [0, 0, 0],
-    [0, b, 0],
-    [b / 2, 0, 0],
-    [b / 2, b, 0],
-    [0, 0, b / 8],
-    [0, b, b / 8],
-    [b / 2, 0, b / 8],
-    [b / 2, b, b / 8],]
-    ).T
+        [0, 0, 0],
+        [0, b, 0],
+        [b / 2, 0, 0],
+        [b / 2, b, 0],
+        [0, 0, b / 8],
+        [0, b, b / 8],
+        [b / 2, 0, b / 8],
+        [b / 2, b, b / 8],
+    ]
+).T
 # Number of measurement samples
 k = 50
-# Noise standard deviation
-sigma = 1
 # Maximum velocity allowed to the UAV
 v_max = b / 100
 # Distance threshold
@@ -42,9 +41,29 @@ gamma = b / 100
 initial_uav_position = [10, 10, 5]
 # File containing the waypoints
 destinations = readpathfile("Path.csv")
+# Noise seed
+noise_seed = 1
+# Noise Distribution
+noise_distribution = "normal"
+# Noise distribution parameters
+mean = 0
+std = 1
+noise_distribution_parameters = [mean, std]
 
-#%%
+# %%
 # Finally, one can invoke the [`wls`] and function and plot the estimated trajectory:
 
-[estimated_trajectory, true_trajectory] = wls(a_i, n, k, sigma, destinations, initial_uav_position, v_max, tau, gamma)
-plt_obj = plot_trajectories(destinations, [estimated_trajectory], a_i, ['WLS'])
+[estimated_trajectory, true_trajectory] = wls(
+    a_i,
+    n,
+    k,
+    destinations,
+    initial_uav_position,
+    v_max,
+    tau,
+    gamma,
+    noise_seed,
+    noise_distribution,
+    noise_distribution_parameters,
+)
+plt_obj = plot_trajectories(destinations, [estimated_trajectory], a_i, ["WLS"])
